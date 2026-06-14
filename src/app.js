37e7651852573163b018e8008a72252b54438042
Nishanta <nishantabhuyan71@gmail.com>
2026-06-15 00:48:26 +0530

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.use(express.json());
 app.use(cookieParser());
